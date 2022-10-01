06540036484914df251e9cd7862641f13a605ec8
Gaurav Jain <gj979986@gmail.com>
2022-10-01 10:37:42 +0530

    Update 91 - Decode Ways.cpp

--- Medium/91 - Decode Ways.cpp
+++ Medium/91 - Decode Ways.cpp
@@ -4,8 +4,10 @@ public:
         int n = s.size();
         vector<int>dp(n+10);
         
-        dp[0] = 1;
-        dp[1] = ((s[0] - '0')==0) ? 0:1;
+        dp[0] = 1; /*a - to check for double digit*/
+        dp[1] = ((s[0] - '0')==0) ? 0:1; /*b - to check for single digit*/
+        
+        /*These a and b will increment as we move on - thats why dp is used*/
         
         for(int i=2; i<=n; i++)
         {
