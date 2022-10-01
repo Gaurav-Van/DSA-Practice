4b3c19d5a142bdea6225e5f72fba9a5133afb9ba
Gaurav Jain <gj979986@gmail.com>
2022-10-01 16:18:26 +0530

    Create 80 - Remove Duplicates from Sorted Array II.py

---
+++ Medium/80 - Remove Duplicates from Sorted Array II.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in nums:
+            if nums.count(i) > 2:
+                while nums.count(i) > 2:
+                    nums.remove(i)
+        
+        return len(nums)
