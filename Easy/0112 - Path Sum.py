5167887c0dde61091863a5cd85793d395cfc1eea
Gaurav Jain <gj979986@gmail.com>
2022-10-04 20:46:52 +0530

    Create 0112 - Path Sum.py

---
+++ Easy/0112 - Path Sum.py
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        
+        if(root == NULL)
+        {
+            return false;
+        }
+        
+        if((root->val == targetSum) && (root->left == NULL) && (root->right == NULL))
+        {
+            return true;
+        }
+        
+        targetSum = targetSum - root->val;
+        
+        return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);     
+    }
+};
